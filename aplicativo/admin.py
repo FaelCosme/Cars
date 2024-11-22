e9c0e532d1ee8d7451ff15a44dae9418f634f3da
FaelCosme <rafael.cross19@gmail.com>
2024-11-22 16:04:42 -0300

    Tirei o "user" do link

--- aplicativo/admin.py
+++ aplicativo/admin.py
@@ -3,7 +3,7 @@ from aplicativo.models import Car
 
 class CarFilter(admin.ModelAdmin):
     list_display = ("id", "user", "model", "brand", "year")
-    list_display_links = ("id", "user", "model", "brand", "year")
+    list_display_links = ("id", "model", "brand", "year")
     list_filter = ("model", "year")
     search_fields = ("model", "brand")
     
